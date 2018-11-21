77599b6bf5e1c1b8378406b65132f890802872bb
SkyeN <skye@dames.es>
2018-11-21 15:45:18 -0800

    add an endpoint to create a todo item

--- app.js
+++ app.js
@@ -15,7 +15,31 @@ app.get("/api/v1/todos", (req, res) => {
     todos: db
   });
 });
+//Create todo
 
+app.post("/api/v1/todos", (req, res) => {
+  if (!req.body.title) {
+    return res.status(400).send({
+      success: "false",
+      message: "title is required"
+    });
+  } else if (!req.body.description) {
+    return res.status(400).send({
+      success: "false",
+      message: "description is required"
+    });
+  }
+  const todo = {
+    id: db.length + 1,
+    title: req.body.title,
+    description: req.body.description
+  };
+  db.push(todo);
+  return res.status(201).send({
+    success: "true",
+    message: "todo added successfully"
+  });
+});
 //Get a single todo
 app.get("/api/v1/todos/:id", (req, res) => {
   const id = parseInt(req.params.id, 10);
