1237b24c83658bb2f4ac47c7ad7eaf8369bbc41f
SkyeN <skye@dames.es>
2018-11-21 16:03:59 -0800

    add method to update a todo item

--- app.js
+++ app.js
@@ -79,6 +79,51 @@ app.delete("/api/v1/todos/:id", (req, res) => {
   });
 });
 
+app.put("/api/v1/todos/:id", (req, res) => {
+  const id = parseInt(req.params.id, 10);
+  let todoFound;
+  let itemIndex;
+
+  db.map((todo, index) => {
+    if (todo.id === id) {
+      todoFound = todo;
+      itemIndex = index;
+    }
+  });
+
+  if (!todoFound) {
+    return res.status(400).send({
+      success: "false",
+      message: "todo not found"
+    });
+  }
+
+  if (!req.body.title) {
+    return res.status(400).send({
+      success: "false",
+      message: "title is required"
+    });
+  } else if (!req.body.description) {
+    return res.status(400).send({
+      success: "false",
+      message: "description is required"
+    });
+  }
+
+  const updatedTodo = {
+      id:todoFound.id,
+      title:req.body.title || todoFound.title,
+      description:req.body.description || todoFound.description
+  };
+
+  db.splice(itemIndex,1,updatedTodo);
+
+  return res.status(200).send({
+      success:'true',
+      message:'todo updated successfully',
+      updatedTodo
+  })
+});
 const PORT = 5000;
 
 // creating a server
